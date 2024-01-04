3378f123a20cf73fd3493f011d116c139d2f643d
Dario Anongba Varela <dario.anongba@gmail.com>
2024-01-04 17:26:24 +0100

    postgres added and working

--- migrations/20231228201258_lightning_addresses_table.sql
+++ migrations/20231228201258_lightning_addresses_table.sql
@@ -1,3 +1,7 @@
+CREATE OR REPLACE FUNCTION update_timestamp() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ language 'plpgsql';
 create table "lightning_addresses" (
     id uuid primary key default gen_random_uuid(),
     user_id varchar(255) unique not null,
