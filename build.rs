3e9888736d034a228e995ceb5c870185bc8e5cba
Dario Anongba Varela <dario.anongba@gmail.com>
2024-07-02 15:32:42 +0200

    experimental protoc

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_server(false).compile(
-        &["src/infra/lightning/cln/proto/node.proto"],
-        &["src/infra/lightning/cln/proto"],
-    )?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .build_server(false)
+        .compile(
+            &["src/infra/lightning/cln/proto/node.proto"],
+            &["src/infra/lightning/cln/proto"],
+        )?;
     println!(
         "cargo:info=Generated CLN files from proto at {:?}",
         std::env::var("OUT_DIR").unwrap()
